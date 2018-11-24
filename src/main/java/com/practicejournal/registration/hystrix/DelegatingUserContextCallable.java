3ac438728b4442e23a1fd0d0a715b0cf2c8598bb
Melvin Jones <Melvin.Jones@jefferson.edu>
2018-11-23 23:03:40 -0500

    initial import

---
+++ src/main/java/com/practicejournal/registration/hystrix/DelegatingUserContextCallable.java
@@ -0,0 +1,34 @@
+package com.practicejournal.registration.hystrix;
+
+import java.util.concurrent.Callable;
+
+import com.practicejournal.registration.utils.UserContext;
+import com.practicejournal.registration.utils.UserContextHolder;
+
+
+public final class DelegatingUserContextCallable<V> implements Callable<V> {
+    private final Callable<V> delegate;
+    private UserContext originalUserContext;
+
+    public DelegatingUserContextCallable(Callable<V> delegate,
+                                         UserContext userContext) {
+        this.delegate = delegate;
+        this.originalUserContext = userContext;
+    }
+
+    public V call() throws Exception {
+        UserContextHolder.setContext( originalUserContext );
+
+        try {
+            return delegate.call();
+        }
+        finally {
+            this.originalUserContext = null;
+        }
+    }
+
+    public static <V> Callable<V> create(Callable<V> delegate,
+                                         UserContext userContext) {
+        return new DelegatingUserContextCallable<V>(delegate, userContext);
+    }
+}
